cf7bc0c780c7c89b678a45a048620cee5fe39cb0
Fraz <frazikram1@gmail.com>
2020-10-21 19:46:52 -0400

    Removed unneeded code and refacotred so Business lists takes props from businesses

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./Business.css";
 
 class Business extends React.Component {
@@ -10,19 +11,19 @@ class Business extends React.Component {
             alt=""
           />
         </div>
-        <h2>{business.name}</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
             <p>
-              {business.state} {business.category}
+              {this.props.business.state} {this.props.business.category}
             </p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{this.props.business.rating}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
